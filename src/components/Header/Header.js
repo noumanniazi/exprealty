ceae4decc098d895e152b1d83c5225482a17051b
noumanniazi <noumanaamir@Nouman.local>
2020-01-06 17:10:16 +0500

    added home link in logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import logo from "../../assets/logo.png";
-import "./Header.scss";
+import logo from '../../assets/logo.png';
+import './Header.scss';
 
 const Header = () => {
   return (
     <div className="nav-bar">
-      <img className="logo-img" src={logo} />
+      <a href="https://www.exprealtyretirementplan.com/">
+        <img className="logo-img" src={logo} alt="EXPRealty"/>
+      </a>
       <ul className="nav-items">
-          <li className="nav-item"><a href="https://www.exprealtyretirementplan.com/">HOME</a></li>
+        <li className="nav-item">
+          <a href="https://www.exprealtyretirementplan.com/">HOME</a>
+        </li>
       </ul>
     </div>
   );
